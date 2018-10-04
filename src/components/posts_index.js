7c7d04585040ef336450ad48813af1997d22bc6d
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-04 07:13:42 +0300

    add navigation from / to /posts/new

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import _ from 'lodash';
 import {connect} from 'react-redux';
-
+import {Link} from 'react-router-dom'
 import { fetchPosts } from './../actions/index';
 
 
@@ -13,8 +13,8 @@ class PostsIndex extends Component{
         return _.map(this.props.posts,post=>{
             return(
                 <li className='list-group-item' key={post.id}>
-                {post.title}
-            </li>
+                    {post.title}
+                </li>
             )
            
         })
@@ -23,6 +23,9 @@ class PostsIndex extends Component{
         // console.log(this.props.posts)
         return(
             <div>
+                <div className='text-xs-right'>
+                    <Link className='btn btn-primary' to='/posts/new'>Add a post</Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     {this.renderPosts()}
