f70635212774b6c994a6586d9710c139a0c8cc87
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-05 07:36:00 +0300

    connect action and reducer in post_show

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import{fetchPost} from '../actions/index';
 
 class PostShow extends Component{
+    componentDidMount(){
+        //provided by react-router params object contains all wild cards inside a url
+        const {id}=this.props.match.params.id;
+        console.log( this.props.match);
+
+        this.props.fetchPost(id);
+    }
     render(){
         return(
             <diV>
                 Post Show
             </diV>
-        )
+        );
     }
 } 
 
-export default PostShow;
+//take posts from state first argument is app state second arg ownProps this.props===ownProps
+function mapStateToProps({posts},ownProps){
+    //select just one post not all list of posts
+ return{post:ownProps.match.params.id}
+}
+
+export default connect(mapStateToProps,{fetchPost})(PostShow);
