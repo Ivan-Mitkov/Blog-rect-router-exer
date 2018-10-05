8c9da809bab75d5a255b124caef1bc4955770b5e
Ivan Mitkov <ivan.mitkov@gmail.com>
2018-10-05 08:10:05 +0300

    linking posts and post

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions/index';
 
 class PostShow extends Component {
-    componentDidMount() {
-        //provided by react-router params object contains all wild cards inside a url
-        const { id } = this.props.match.params;
-        // console.log(this.props.match);
 
-        this.props.fetchPost(id);
+    componentDidMount() {
+        //in order to not download twice see network for additional request if if is not here
+        if (!this.props.post) {
+            //provided by react-router params object contains all wild cards inside a url
+            const { id } = this.props.match.params;
+            // console.log(this.props.match);
+
+            this.props.fetchPost(id);
+        }
     }
+
     render() {
         const { post } = this.props;
         if (!post) {
@@ -19,7 +25,8 @@ class PostShow extends Component {
         }
         return (
             <div>
-                {console.log(post)}
+                <Link to="/">Show all posts</Link>
+                {/* {console.log(post)} */}
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,27 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import _ from 'lodash';
-import {connect} from 'react-redux';
-import {Link} from 'react-router-dom'
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
 import { fetchPosts } from './../actions/index';
 
 
-class PostsIndex extends Component{
-    componentDidMount(){
+class PostsIndex extends Component {
+    componentDidMount() {
         this.props.fetchPosts();
     }
-    renderPosts(){
-        return _.map(this.props.posts,post=>{
-            return(
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
                 <li className='list-group-item' key={post.id}>
-                    {post.title}
+                    <Link to={`/posts/${post.id}`}>
+                        {post.title}
+                    </Link>
                 </li>
             )
-           
+
         })
     }
-    render(){
+    render() {
         // console.log(this.props.posts)
-        return(
+        return (
             <div>
                 <div className='text-xs-right'>
                     <Link className='btn btn-primary' to='/posts/new'>Add a post</Link>
@@ -34,7 +36,7 @@ class PostsIndex extends Component{
         );
     }
 }
-function mapStateToProps(state){
-    return{posts:state.posts}
+function mapStateToProps(state) {
+    return { posts: state.posts }
 }
-export default connect(mapStateToProps,{fetchPosts})(PostsIndex) ;
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
